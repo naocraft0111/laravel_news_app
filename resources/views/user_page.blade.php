50f6ad5f5c5d954fb5f3e34b9581384922ff3592
naocraft0111 <naoki1306@gmail.com>
2022-11-01 17:21:22 +0900

    記事一覧にサムネイル画像を表示

--- resources/views/user_page.blade.php
+++ resources/views/user_page.blade.php
@@ -12,8 +12,21 @@
                     <a href="{{ url('u/' . $user->display_name . '/' . $user->id) }}">
                         <h5>{{ $news->title}}</h5>
                     </a>
-                    <p>{{ $news->description }}</p>
-                    <p>create: {{ $news->created_at->format("Y-m-d H:i:s") }}</p>
+                    <div class="row">
+                        @if ($news->thumbnail_url || $news->image_url)
+                        <div class="col">
+                            @if ($news->thumbnail_url)
+                            <img src="{{ Storage::url($news->thumbnail_url) }}" style="width: 150px;" />
+                            @elseif($news->image_url)
+                            <img src="{{ Storage::url($news->image_url) }}" style="width: 150px;" />
+                            @endif
+                        </div>
+                        @endif
+                        <div class="col">
+                            <p>{{ $news->description }}</p>
+                            <p>create: {{ $news->created_at->format("Y-m-d H:i:s") }}</p>
+                        </div>
+                    </div>
                 </li>
                 @endforeach
             </ul>

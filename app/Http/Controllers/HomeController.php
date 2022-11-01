29002da0b5d247ace094287760b71ca22e069788
naocraft0111 <naoki1306@gmail.com>
2022-11-01 13:56:51 +0900

    記事一覧機能の作成

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\NewsEntry;
+use Illuminate\Support\Facades\Auth;
+
 class HomeController extends Controller
 {
     /**
@@ -23,6 +26,17 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        // ログイン中のユーザー
+        $user = Auth::user();
+
+        // NewsEntryのうち、user_idが現在ログイン中のユーザーのIDと一致するものを取得
+        $news_list = $user->newsEntry()
+                    ->orderBy("id", "desc")
+                    ->paginate(10);
+                    
+        return view('home', compact(
+            'user',
+            'news_list'
+        ));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,9 +13,30 @@
                             {{ session('status') }}
                         </div>
                     @endif
-                        <div class="mb-3">
-                            <a href="{{ url('/news/create') }}" class="btn btn-primary">記事作成</a>
-                        </div>
+
+                    <div class="mb-3">
+                        <a href="{{ url('/news/create') }}" class="btn btn-primary">記事作成</a>
+                    </div>
+
+                    <ul class="list-group">
+                        @foreach ($news_list as $news)
+                        <li class="list-group-item">
+                            <a href="{{ url('news/edit/' . $news->id) }}">
+                                <h5>{{ $news->title }}</h5>
+                            </a>
+                            <p>{{ $news->description }}</p>
+                            <p>create: {{ $news->created_at->format("Y-m-d H:i:s") }}</p>
+                            <p class="text-right">
+                                <a href="{{ url('u/' . $user->display_name . '/' . $news->id) }}" class="btn btn-outline-secondary">確認</a>
+                            </p>
+                        </li>
+                        @endforeach
+                    </ul>
+
+                    <div class="mt-3">
+                        {{ $news_list->links() }}
+                    </div>
+                    
                 </div>
             </div>
         </div>
